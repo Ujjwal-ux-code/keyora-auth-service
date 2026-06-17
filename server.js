5b674ebf2608ad3b7e3bd1cc5f7e640d0ac7abe1
Ujjwal Nishad <ujjwalnishad143@gmail.com>
2026-06-18 00:59:15 +0530

    Initial Express server and MongoDB setup

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Keyora Running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
